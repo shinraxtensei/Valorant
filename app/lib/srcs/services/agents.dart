99304d58fe25497feb640c6615e096321ed2461b
anasshouarii@gmail.com <anasshouarii@gmail.com>
2024-02-12 16:31:46 +0100

    Add internet permission and fetch agents from server

--- app/lib/srcs/services/agents.dart
+++ app/lib/srcs/services/agents.dart
@@ -1,1 +1,15 @@
-//fetching agents from the server
+import 'dart:convert';
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+import 'package:valorant/srcs/models/agent.dart';
+
+Future<Agent> fetchAgents() async {
+  final response =
+      await http.get(Uri.parse('${dotenv.env['API_BASE_URL']}/agents'));
+  if (response.statusCode == 200) {
+    return Agent.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+  } else {
+    throw Exception('Failed to load album');
+  }
+}
